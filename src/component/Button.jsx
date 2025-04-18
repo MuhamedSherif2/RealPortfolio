f14c47475a939b7d4cd4c1c257563702e15eb0d6
MuhamedSherif2 <your-email@example.com>
2025-04-18 15:38:16 +0200

    button component

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -2,7 +2,7 @@ function Button(props) {
     console.log(props)
     return (
         <button className={`w-[150px] py-[6px] px-3 ${props.bg} ${props.mt} mdl:mt-0 rounded mx-auto transition duration-300  hover:text-white hover:${props.hoverBg}`}>
-            <span className="flex items-center gap-1 text-[20px] font-semibold">{props.name}{props.icon}</span>
+            <span className="flex items-center gap-1 text-[20px] font-semibold text-center justify-center">{props.name}{props.icon}</span>
         </button>
     )
 }
